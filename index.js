681db703d9a81fd78feff297844d13070ce967ec
prajwal1327 <118236029+prajwal1327@users.noreply.github.com>
2024-02-07 17:16:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
 const functions = require('@google-cloud/functions-framework');
-const GcmSynthetics = require('@google-cloud/synthetics-sdk-broken-links');
+const { runBrokenLinksHandler } = require('@google-cloud/synthetics-sdk-broken-links');
 
 const urls = [
   'https://assurx.ikshealth.com',
-  'https://www.google.com',
-  'https://www.wikipedia.org'
+  'https://careuatdmda.ikshealth.com/',
+  'https://glmgoptimix.ikshealth.com/'
 ];
 
-async function runBrokenLinksHandler(url) {
-  const options = {
-    origin_uri: url,
-    link_limit: 25,
-    query_selector_all: 'a,img,script',
-    wait_for_selector: '',
-    get_attributes: ["href","src"],
-    link_order: 'FIRST_N',
-    link_timeout_millis: 30000,
-    max_retries: 0,
-    per_link_options: {}
-  };
-  
+async function checkBrokenLinks(url) {
   try {
-    const result = await GcmSynthetics.runBrokenLinksHandler(options);
+    const options = {
+      origin_uri: url,
+      link_limit: 25,
+      query_selector_all: 'a,img,script',
+      wait_for_selector: '',
+      get_attributes: ["href","src"],
+      link_order: 'FIRST_N',
+      link_timeout_millis: 30000,
+      max_retries: 0,
+      per_link_options: {}
+    };
+    const result = await runBrokenLinksHandler(options);
     console.log(`Broken links check for ${url} successful:`, result);
     return result;
   } catch (error) {
@@ -32,14 +31,20 @@ async function runBrokenLinksHandler(url) {
 
 async function handleMultipleUrls(urls) {
   try {
-    const results = await Promise.all(urls.map(url => runBrokenLinksHandler(url)));
+    const results = await Promise.all(urls.map(url => checkBrokenLinks(url)));
     console.log('All URL checks completed:', results);
   } catch (error) {
     console.error('Error handling multiple URLs:', error);
+    throw error;
   }
 }
 
-functions.http('BrokenLinkChecker', (req, res) => {
-  handleMultipleUrls(urls);
-  res.status(200).send('Multiple URL checks initiated.');
+functions.http('BrokenLinkChecker', async (req, res) => {
+  try {
+    await handleMultipleUrls(urls);
+    res.status(200).send('All URL checks completed.');
+  } catch (error) {
+    console.error('Error handling multiple URLs:', error);
+    res.status(500).send('Internal Server Error.');
+  }
 });
